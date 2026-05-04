15f0ea222ad6b3826aba9040f72420994bd27d8d
Russell-Cannon <russellrcannon@gmail.com>
2026-05-04 17:57:30 -0600

    Cleanup Player script

--- Player.cs
+++ Player.cs
@@ -14,16 +14,14 @@ public struct MovementSettings
 
 public partial class Player : CharacterBody3D
 {
-	[Export] double gravity = 20, jumpForce = 7, roofRebound = 1f, friction = 6, slideForce = 12, slideFriction = 0.125f, terminalVelocity = 50f, brakeAmount = 0.125f, wallJumpMult = 1.5f;
+	[Export] double gravity = 20, jumpForce = 7, roofRebound = 1f, friction = 6, slideForce = 12;
 	[Export] Camera3D Camera;
 	[Export] CollisionShape3D Shape;
 	public Vector2 MoveInput;
 	Vector3 groundSurfaceNormal = Vector3.Up;
 	float airControl = 0.3f;
 	MovementSettings groundSettings = new(7, 14, 10), airSettings = new(7, 2, 2), strafeSettings = new(1, 50, 50);
-	Gated canShoot = new(0.4f), canReload = new(2.6f), canSlide = new(1.125f), canLash = new(0.125f);
 	Buffer jump = new(0.125f);
-	public bool Crouched = false;
 	const float MASS = 70;
 
 
@@ -72,8 +70,6 @@ public partial class Player : CharacterBody3D
 			groundSurfaceNormal = Vector3.Up;
 		}
 		Velocity += Vector3.Down * (float)(gravity * delta); //v = a*t //Apply gravity
-		if (Velocity.Length() > terminalVelocity)
-			Velocity = Velocity.Normalized() * (float)terminalVelocity;
 
 		MoveAndSlide();
 
@@ -82,69 +78,75 @@ public partial class Player : CharacterBody3D
 			Collide(col);
 		}
 	}
-    public override void _Process(double delta) {
-		ClampHead();
-    }
 
 //physics
     void Collide(KinematicCollision3D collision) {
+		//Get the normal of the current surface
 		Vector3 normal = collision.GetNormal();
 
+		//If possible: push rigidbodies back as they push on us
 		if (collision.GetCollider() is RigidBody3D rb)
 			rb.ApplyCentralForce(MASS * Velocity.Project(normal));
 
-		if (Vector3.Up.Dot(normal) < 0) { // Bounce off roof
-			Addforce(normal * (float)roofRebound * Vector3.Down.Dot(normal));
+		//If the normal is facing downwards: bound off the surface.
+		if (Vector3.Up.Dot(normal) < 0) {
+			AddForce(normal * (float)roofRebound * Vector3.Down.Dot(normal));
 		} else if (Vector3.Up.Dot(normal) > 0.5f) {
+			//If the normal is facing mostly upwards, cancel the affects of gravity
 			groundSurfaceNormal = normal;
 		}
 	}
 	private void AirMove(double delta)
 	{
-		float accel;
+		float Acceleration;
 
+		// Get the direction the player wants to go in place
 		var wishdir = (MoveInput.X * GlobalBasis.X) + (MoveInput.Y * GlobalBasis.Z);
-
+		// The players wants to go as fast as possible times whether or not they even want to move
 		float wishspeed = wishdir.Length() * airSettings.MaxSpeed;
-		wishdir = wishdir.Normalized();
-		// CPM Air control
-		if (Velocity.Dot(wishdir) < 0) {
-			accel = airSettings.Deceleration;
-		} else {
-			accel = airSettings.Acceleration;
-		}
+
 		// If the player is ONLY strafing left or right
 		if (MoveInput.Y == 0 && MoveInput.X != 0) {
+			//Clamp desired speed to max speed
 			if (wishspeed > strafeSettings.MaxSpeed) {
 				wishspeed = strafeSettings.MaxSpeed;
 			}
-			accel = strafeSettings.Acceleration;
+			Acceleration = strafeSettings.Acceleration;
+		} else { // Otherwise moving forwards/backwards or diagonnaly
+			//If moving forwards: accelerate. If moving backwards: decelerate
+			if (Velocity.Dot(wishdir) > 0) {
+				Acceleration = airSettings.Acceleration;
+			} else {
+				Acceleration = airSettings.Deceleration;
+			}
 		}
-		Accelerate(wishdir, wishspeed, accel, delta);
 
+		//Accelerate 
+		Accelerate(wishdir, wishspeed, Acceleration, delta);
 
-		if (airControl > 0) {
-			AirControl(wishdir, wishspeed, delta);
-		}
+		//Apply air control 
+		AirControl(wishdir, wishspeed, delta);
 	}
 	private void AirControl(Vector3 targetDir, float wishspeed, double delta) {
-		// Air control occurs when the player is in the air, it allows players to move side 
-		// to side much faster rather than being 'sluggish' when it comes to cornering.
+		// Air strafe the player is in the air
+		// Allows players to move turn mid-air (corner)
 
 		// Only control air movement when moving forward or backward and actually moving
-		if (Mathf.Abs(MoveInput.Y) < 0.001 || Mathf.Abs(wishspeed) < 0.001)
-		{
+		if (MoveInput.Y == 0 || Mathf.Abs(wishspeed) < 0.001)
 			return;
-		}
-		Vector3 ySpeed = Velocity.Project(Vector3.Down); //record how much velocity is upward
-		Velocity -= Velocity.Project(Vector3.Down); //remove all velocity downwards
 
+		//Store how much velocity is facing downwards and remove it
+		Vector3 ySpeed = Velocity.Project(Vector3.Down); 
+		Velocity -= Velocity.Project(Vector3.Down); 
+
+		//Store the current speed then remove it from the player
 		float speed = Velocity.Length();
 		Velocity = Velocity.Normalized();
 
+		//Get the distance between where the player is going and where they want to go
 		float dot = Velocity.Dot(targetDir);
 		double k = 32;
-		k *= airControl * dot * dot * delta;
+		k *= airControl * dot * dot * delta; //!!!
 
 		Vector3 xSpeed = Velocity.Project(GlobalBasis.X);
 		Vector3 zSpeed = Velocity.Project(GlobalBasis.Z); 
@@ -161,85 +163,85 @@ public partial class Player : CharacterBody3D
 			Velocity = Velocity.Normalized();//change direction
 		}
 
-		Velocity *= speed;//restore magnitude
-		Velocity += ySpeed; //preserves y velocity b/c the player could be falling
+		//Restore speed
+		Velocity *= speed;
+		//Restore downwards velocity
+		Velocity += ySpeed;
 	}
 
 	private void GroundMove(double delta) {
 		// Handle ground movement.
-		// Do not apply friction if the player is queueing up the next jump to maintain speed
-		if (Crouched) {
-			if (IsOnFloor()) //add force downhill
-				Addforce((groundSurfaceNormal - Vector3.Down) * (float)gravity * MASS, delta);
-			ApplyFriction(slideFriction, delta);
-		}
-		else
-			ApplyFriction(1.0f, delta);
+		ApplyFriction(delta);
 
+		//Convert the players input to a vector pointing where the player wants to go
 		Vector3 realDir = (MoveInput.X * GlobalBasis.X) + (MoveInput.Y * GlobalBasis.Z);
+		//Align that vector such that walking forward on a slope is actually partially walking downwards
 		realDir = GodotMath.AlignUpToNormal(groundSurfaceNormal, realDir).Z;
-		var wishspeed = MoveInput.Length() * groundSettings.MaxSpeed;
-		if (Crouched) realDir *= 0.125f;
-		Accelerate(realDir, wishspeed, groundSettings.Acceleration, delta);
+		
+		//Accelerate the player towards where they want to go
+		Accelerate(realDir, MoveInput.Length() * groundSettings.MaxSpeed, groundSettings.Acceleration, delta);
 	}
 
-	private void ApplyFriction(double t, double delta) {
+	private void ApplyFriction(double delta) {
+		//Get speed
 		float speed = Velocity.Length();
-		float control = speed < groundSettings.Deceleration ? groundSettings.Deceleration : speed;
-		float newSpeed = speed - (float)(control * friction * delta * t);
-		newSpeed = newSpeed < 0 ? 0 : newSpeed / speed;
 
-		Velocity *= newSpeed;
+		//Raise friction in the lower bound of speed to keep movement snappy
+		float control = speed < groundSettings.Deceleration ? groundSettings.Deceleration : speed;
+		//Get amount of speed to lose based on current speed * friction per second
+		float speedLoss = (float)(control * friction * delta);
+
+		//Clamp newspeed to avoid slowing down into the opposite direction.
+		if (speedLoss > speed) 
+			speedLoss = speed;
+
+		//Lower velocity by the ratio of new speed to current speed
+		Velocity *= (speed - speedLoss) / speed;
 	}
 
 	private void Accelerate(Vector3 targetDir, double targetSpeed, double accel, double delta) {
 		// Calculates acceleration based on desired speed and direction.
-		// Because this is based on Quake, there is some an oversight causes airstrafing to behave the way it does
-		double currentspeed = Velocity.Dot(targetDir); //This is where the oversight is. When we chose to move perpendicular to velocity, our speed is read as 0.
+		// Get how much of our velocity is towards were we want to go.
+		double currentspeed = Velocity.Dot(targetDir);
+		// Get how much speed we have left to gain.
 		double addspeed = targetSpeed - currentspeed;
+		// If going faster than we want: don't slow down
 		if (addspeed <= 0)
-		{
 			return;
-		}
 
+		// Get how much speed should be added per second
 		double accelspeed = accel * delta * targetSpeed;
+		// Don't add enough speed to overpass the max speed
 		if (accelspeed > addspeed)
-		{
 			accelspeed = addspeed;
-		}
+
+		// Add velocity
 		Velocity += (float)accelspeed*targetDir;
 	}
 
-	public void Addforce(Vector3 force) {//impulse
-		Addforce(force, 1);
+	public void AddForce(Vector3 force) {//impulse
+		AddForce(force, 1);
 	}
-	public void Addforce(Vector3 force, double delta) {//real force
+	public void AddForce(Vector3 force, double delta) {//real force
 		/*	v(t) = |f(t)/m dt
 			v(t) = F*t/m + C	*/
 		Velocity += force*(float)delta/MASS;
 	}
-	private bool CanMoveInDirection(Vector3 direction)
-	{
-		if (!IsOnFloor() && !IsOnWall()) return true;
-
-		return GetFloorNormal().Dot(direction) > -0.5f && GetWallNormal().Dot(direction) > -0.5f; // Allow movement if not directly against the normal
-	}
 	
 //input
 	public void Jump() {
-		if (IsOnFloor()) {//floor jumps always bounce upward
+		if (IsOnFloor()) {
+			// If already going down : cancel velocity
 			if (Velocity.Dot(Vector3.Down) > 0)
-				Velocity += -Velocity.Project(Vector3.Down); // If already going down : cancel velocity
+				Velocity += -Velocity.Project(Vector3.Down); 
+			// Add jump force upwards
 			Velocity += (float)jumpForce * Vector3.Up;
-		} else if (IsOnWall()) { //wall jumps are towards normal
-			Vector3 normal = GetWallNormal();
-			if (Velocity.Dot(normal) > 0)
-				Velocity += -Velocity.Project(normal);
-			Velocity += (float)(jumpForce * wallJumpMult) * normal;
 		}
+		//Stop the jump buffer here
 		jump.Cancel();
 	}
 	void ClampHead() {
+		//Stop the camera from looking too far up or down
 		Camera.Rotation = new Vector3(Mathf.Clamp(Camera.Rotation.X, -GodotMath.HALF_PI, GodotMath.HALF_PI), Camera.Rotation.Y, Camera.Rotation.Z);
 	}
 }
