3abc0bce4bd99f14ddab8d2fa9962dcd930593d9
Russell-Cannon <russellrcannon@gmail.com>
2026-07-24 20:02:06 -0600

    Interpolate positions

--- scripts/Actor.cs
+++ scripts/Actor.cs
@@ -6,13 +6,18 @@ public partial class Actor : CharacterBody3D
 {
     [Export] public Label3D nameTag;
     public ulong ID { get; private set;}
+    public Vector3 KnownPosition;
     public void SetID(ulong id)
     {
         ID = id;
         nameTag.Text = Steam.GetFriendPersonaName(id);
     }
-    public void MoveActor(Vector3 position)
+    public void MoveActor(Vector3 _Position)
     {
-        GlobalPosition = position;
+        KnownPosition = _Position;
+    }
+    public override void _Process(double delta)
+    {
+        GlobalPosition = Lerp.LerpHalfLife(GlobalPosition, KnownPosition, (float)delta, 0.0125f);
     }
 }
