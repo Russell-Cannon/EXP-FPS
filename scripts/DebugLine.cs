c5d95018e79239f9a73f069e81831f3a43e5db60
Russell-Cannon <russellrcannon@gmail.com>
2026-08-05 14:42:09 -0600

    Allow multiple debug lines to be drawn per frame

--- scripts/DebugLine.cs
+++ scripts/DebugLine.cs
@@ -1,48 +1,59 @@
 using Godot;
 using System;
+using System.Collections.Generic;
 
 public partial class DebugLine : MeshInstance3D
 {
-	public static DebugLine I;
-    public int lastingTime = 0;
+	public static DebugLine Instance;
     ImmediateMesh mesh = new();
     StandardMaterial3D meshMaterial = new() {
         DisableReceiveShadows = true,
         NoDepthTest = true,
-        AlbedoColor = new Color(0x00000011)
+        AlbedoColor = new Color(0x00000011),
     };
     Camera3D cam = null;
-    int lastDrewFrame;
-    public override void _Ready() {
-        I = this;
+    List<Vector3[]> Lines = new();
+
+    public override void _Ready() 
+    {
+        Instance = this;
         Mesh = mesh;
         cam = GetViewport().GetCamera3D();
     }
-    public override void _Process(double delta) {
-        if (Engine.GetFramesDrawn() > lastDrewFrame + lastingTime)
-            mesh.ClearSurfaces();
-    }
-    public void DrawLine(Vector3 start, Vector3 end, float LineThickness = 0.00625f, bool flat = true) {
-        if (cam == null) {
+    public override void _Process(double delta) 
+    {
+        if (Lines.Count == 0) return;
+        if (cam == null || !IsInstanceValid(cam)) {
             cam = GetViewport().GetCamera3D();
             return;
         }
+        
+        GlobalPosition = Vector3.Zero;
 
-        lastDrewFrame = Engine.GetFramesDrawn();
-
-        GlobalPosition = start;
         mesh.ClearSurfaces();
 
-        mesh.SurfaceBegin(Mesh.PrimitiveType.TriangleStrip, meshMaterial);
-        
-        for (int i = 0; i < 2; i++) {
-            Vector3 position = start.Lerp(end, i);
-            Vector3 dir = start.DirectionTo(cam.GlobalPosition).Cross(start.DirectionTo(end)).Normalized() * LineThickness * (flat ? cam.GlobalPosition.DistanceTo(position) : 1f)/2f;//Fix sizes decreasing over distance, if flat
-            for (int j = 0; j < 2; j++) {
-                mesh.SurfaceAddVertex(ToLocal(start.Lerp(end, i) + dir*(j*2f - 1)));
+        foreach (Vector3[] Line in Lines)
+        {
+            mesh.SurfaceBegin(Mesh.PrimitiveType.TriangleStrip, meshMaterial);
+            for (int i = 0; i < 2; i++) 
+            {
+                Vector3 position = Line[0].Lerp(Line[1], i);
+                Vector3 dir = Line[0].DirectionTo(cam.GlobalPosition).Cross(Line[0].DirectionTo(Line[1])).Normalized() * 0.0065f * cam.GlobalPosition.DistanceTo(position)/2f;
+                for (int j = 0; j < 2; j++) {
+                    mesh.SurfaceAddVertex(ToLocal(Line[0].Lerp(Line[1], i) + dir*(j*2f - 1)));
+                }
             }
+            mesh.SurfaceEnd();
         }
 
-        mesh.SurfaceEnd();
+        Lines.Clear();
+    }
+    public void DrawTriad(Vector3 point, float length = 1f) {
+        DrawLine(point - Vector3.Right * length, point + Vector3.Right * length);
+        DrawLine(point - Vector3.Up * length, point + Vector3.Up * length);
+        DrawLine(point - Vector3.Forward * length, point + Vector3.Forward * length);
+    }
+    public void DrawLine(Vector3 start, Vector3 end) {
+        Lines.Add([start, end]);
     }
 }
